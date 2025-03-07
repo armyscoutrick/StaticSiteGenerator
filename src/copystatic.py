1429e57bae4967c81d01415f6610d492021a5ada
armyscoutrick <armyscoutrick@yahoo.com>
2025-03-07 06:31:16 -0500

    Update copystatic.py

--- src/copystatic.py
+++ src/copystatic.py
@@ -9,4 +9,8 @@ def copy_files_recursive(source_dir_path, dest_dir_path):
     for filename in os.listdir(source_dir_path):
         from_path = os.path.join(source_dir_path, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-        print(f __pycache__ copystatic.py htmlnode.py inline_markdown.py main.py markdown_blocks.py test_htmlnode.py test_inline_markdown.py test_markdown_blocks.py test_textnode.py textnode.py {from_path} -
+        print(f" * {from_path} -> {dest_path}")
+        if os.path.isfile(from_path):
+            shutil.copy(from_path, dest_path)
+        else:
+            copy_files_recursive(from_path, dest_path)
