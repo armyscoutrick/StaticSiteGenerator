06e1ccf9045d6b9622cecae7ee28538a942d9247
armyscoutrick <armyscoutrick@yahoo.com>
2025-03-08 10:46:13 -0500

    Update gencontent.py

--- src/gencontent.py
+++ src/gencontent.py
@@ -10,7 +10,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
         if os.path.isfile(from_path):
             dest_path = Path(dest_path).with_suffix(".html")
             generate_page(from_path, template_path, dest_path)
-        else:
+        else: 
+            Path(dest_path).mkdir(parents=True, exist_ok=True)
             generate_pages_recursive(from_path, template_path, dest_path)
 
 
