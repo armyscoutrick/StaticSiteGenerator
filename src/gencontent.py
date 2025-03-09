8e5a12cbb96ad4b70b442dbc4c5137d319037f07
armyscoutrick <armyscoutrick@yahoo.com>
2025-03-09 03:56:49 -0400

    Update gencontent.py

--- src/gencontent.py
+++ src/gencontent.py
@@ -7,9 +7,11 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, bas
     for filename in os.listdir(dir_path_content):
         from_path = os.path.join(dir_path_content, filename)
         dest_path = os.path.join(dest_dir_path, filename)
-        if os.path.isfile(from_path):
-            dest_path = Path(dest_path).with_suffix(".html")
-            generate_page(from_path, template_path, dest_path, basepath)
+        if os.path.isfile(from_path):  
+    
+            if filename.endswith(".md"):
+                dest_path = os.path.splitext(dest_path)[0] + ".html"
+                generate_page(from_path, template_path, dest_path)
         else:
             generate_pages_recursive(from_path, template_path, dest_path, basepath)
 
